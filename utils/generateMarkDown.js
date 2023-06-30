a6c7e8ed76f58feadf27079e1cebb54632c8d644
terenceDev <terence.arseneault@hotmail.com>
2023-06-30 13:41:11 -0300

    added the licensing logic in getnerateMarkDown file

--- utils/generateMarkDown.js
+++ utils/generateMarkDown.js
@@ -1,14 +1,53 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let badge = "";
+  if (license === "Apache") {
+    badge =
+      "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+  } else if (license === "MIT") {
+    badge =
+      "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  } else if (license === "BSD") {
+    badge =
+      "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+  } else if (license === "Boost") {
+    badge =
+      "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
+  } else {
+    badge = "";
+  }
+  return badge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let licenseLink = "";
+  if (license === "Apache") {
+    licenseLink = " https://www.apache.org/licenses/LICENSE-2.0.txt";
+  } else if (license === "MIT") {
+    licenseLink = "http://opensource.org/licenses/mit-license.php";
+  } else if (license === "BSD") {
+    licenseLink = "https://opensource.org/license/bsd-3-clause/";
+  } else if (license === "Boost") {
+    licenseLink = "https://opensource.org/license/bsl-1-0/";
+  } else {
+    licenseLink = "";
+  }
+  return licenseLink;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let licenseSection = "";
+  if (license === "None") {
+    licenseSection = "";
+  } else {
+    licenseSection = `License: ${license}`;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -21,3 +60,4 @@ function generateMarkdown(data) {
 }
 
 module.exports = generateMarkdown;
+"Apache", "MIT", "BSD", "Boost";
